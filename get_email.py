59ec8351c0212181fcf0a5632e73e19fbfbe7b81
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-15 20:25:00 -0400

    3-misol

--- get_email.py
+++ get_email.py
@@ -1,4 +1,6 @@
+import email
 import get_data
+import json
 
 def get_email(data:dict) -> list:
     """
@@ -9,3 +11,12 @@ def get_email(data:dict) -> list:
     Returns:
         list: users email
     """
+    
+    email = []
+    for i in data["results"]:
+        for e,l in dict(i).items():
+            if e == "email":
+                    email.append(l)
+    return email
+
+print(get_email(json.loads(open("randomuser_data.json", 'r').read())))
